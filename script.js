2b1d4954f830214f27b1484114f3715336836b4c
Fernando Ferreira <110427158+FernandoFerreiradeAlmeida@users.noreply.github.com>
2023-11-08 17:00:07 -0300

    calculadora

---
+++ script.js
@@ -0,0 +1,17 @@
+let  expression= '';
+
+function appendNumber(number) {
+    expression += number;
+    document.getElementById('result').value = expression;
+}
+
+function appendOperator(operator) {
+    expression += operator;
+    document.getElementById('result').value = expression;
+}
+
+function calculate() {
+  try {
+   const result = eval(expression);
+   document.getElementyById('result').value = result;
+   expression = '';
